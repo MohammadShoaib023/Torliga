2736f7a0ff709eb809ba4f9026b1d105f25b4962
Mohammad shoaib <eng.mohammadshoaib98@gmail.com>
2024-11-01 11:12:08 +0300

    Initialize dependencies in main with ensureInitialized and async setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'app/app_injection_container.dart';
 import 'app/torliga_app.dart';
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
   runApp(const TorligaApp());
 }
