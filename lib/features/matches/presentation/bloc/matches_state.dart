96efc11d6e0efb2fef73a37a4369652b867cfb83
Mohammad shoaib <eng.mohammadshoaib98@gmail.com>
2024-11-01 15:43:11 +0300

    Refactor MatchesState to include individual status properties for match types, add error handling, and update copyWith method

--- lib/features/matches/presentation/bloc/matches_state.dart
+++ lib/features/matches/presentation/bloc/matches_state.dart
@@ -1,25 +1,54 @@
 import 'package:equatable/equatable.dart';
-
 import '../../../../core/enums/bloc_status.dart';
-import '../../domain/entities/datum_entity.dart';
+import '../../domain/entities/matches_entity.dart';
 
 class MatchesState extends Equatable {
-  final BlocStateStatus status;
-  final List<DatumEntity>? todaysMatches;
-  final List<DatumEntity>? pastMatches;
-  final List<DatumEntity>? upcomingMatches;
+  final BlocStateStatus todaysMatchesStatus;
+  final BlocStateStatus pastMatchesStatus;
+  final BlocStateStatus upcomingMatchesStatus;
+  final String? errorMessage;
+  final MatchesEntity? todaysMatches;
+  final MatchesEntity? pastMatches;
+  final MatchesEntity? upcomingMatches;
 
   const MatchesState({
-    this.status = BlocStateStatus.initial,
-    this.todaysMatches = const [],
-    this.pastMatches = const [],
-    this.upcomingMatches = const [],
+    this.todaysMatchesStatus = BlocStateStatus.initial,
+    this.pastMatchesStatus = BlocStateStatus.initial,
+    this.upcomingMatchesStatus = BlocStateStatus.initial,
+    this.todaysMatches,
+    this.pastMatches,
+    this.upcomingMatches,
+    this.errorMessage,
   });
+
+  MatchesState copyWith({
+    BlocStateStatus? todaysMatchesStatus,
+    BlocStateStatus? pastMatchesStatus,
+    BlocStateStatus? upcomingMatchesStatus,
+    MatchesEntity? todaysMatches,
+    MatchesEntity? pastMatches,
+    MatchesEntity? upcomingMatches,
+    final String? errorMessage,
+  }) =>
+      MatchesState(
+        todaysMatchesStatus: todaysMatchesStatus ?? this.todaysMatchesStatus,
+        pastMatchesStatus: pastMatchesStatus ?? this.pastMatchesStatus,
+        upcomingMatchesStatus:
+            upcomingMatchesStatus ?? this.upcomingMatchesStatus,
+        todaysMatches: todaysMatches ?? this.todaysMatches,
+        pastMatches: pastMatches ?? this.pastMatches,
+        upcomingMatches: upcomingMatches ?? this.upcomingMatches,
+        errorMessage: errorMessage ?? this.errorMessage,
+      );
+
   @override
   List<Object?> get props => [
         todaysMatches,
         pastMatches,
         upcomingMatches,
-        status,
+        todaysMatchesStatus,
+        pastMatchesStatus,
+        upcomingMatchesStatus,
+        errorMessage
       ];
 }
