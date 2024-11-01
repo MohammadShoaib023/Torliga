f0cf3c4a28b5abf7b927cd0a882f09742b76376f
Mohammad shoaib <eng.mohammadshoaib98@gmail.com>
2024-11-01 11:24:14 +0300

    Add test token to ApiEndPoints for development and testing

--- lib/core/constants/app_end_points.dart
+++ lib/core/constants/app_end_points.dart
@@ -3,6 +3,10 @@ class ApiEndPoints {
   static final ApiEndPoints _instance = ApiEndPoints._();
   static ApiEndPoints get instance => _instance;
   static const String baseUrl = "https://staging.torliga.com/api/v1/home";
+  // This Just for testing
+  static const String testToken =
+      "14813|l5WcBvXKig4BrcU0op4LkW7ML7ekVb2vH96Wkm6l00d96ff8";
+
   static const String pastMatches = "/yesterdayMatches";
   static const String upcomingMatches = "/tomorrowMatches";
   static const String todaysMatches = "/todayMatches";
