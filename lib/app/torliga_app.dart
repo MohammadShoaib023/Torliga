40c7d3d7e8ea8f8195ce687a9986fdcb2ac58c47
Mohammad shoaib <eng.mohammadshoaib98@gmail.com>
2024-11-01 11:17:34 +0300

    Wrap TorligaApp with MultiBlocProvider for BLoC state management initialization

--- lib/app/torliga_app.dart
+++ lib/app/torliga_app.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../config/router/app_router.dart';
 import '../core/constants/app_constants.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'app_bloc_providers.dart';
 
 class TorligaApp extends StatelessWidget {
   const TorligaApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      title: AppConstants.appName,
-      theme: ThemeData(textTheme: GoogleFonts.chakraPetchTextTheme()),
+    return MultiBlocProvider(
+      providers: AppBlocProviders.init,
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        title: AppConstants.appName,
+        theme: ThemeData(textTheme: GoogleFonts.chakraPetchTextTheme()),
+      ),
     );
   }
 }
