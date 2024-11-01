f5d75f51c86a955088c78ad96963668af1b26cee
Mohammad shoaib <eng.mohammadshoaib98@gmail.com>
2024-11-01 15:36:36 +0300

    Refactor FetchPastMatchesUseCase to return MatchesEntity type

--- lib/features/matches/domain/usecases/fetch_past_matches_use_case.dart
+++ lib/features/matches/domain/usecases/fetch_past_matches_use_case.dart
@@ -2,11 +2,10 @@ import 'package:dartz/dartz.dart';
 
 import '../../../../core/failure/failure.dart';
 import '../../../../core/usecase/usecase.dart';
-import '../entities/datum_entity.dart';
 import '../entities/matches_entity.dart';
 import '../repository/matches_repository.dart';
 
-class FetchPastMatchesUseCase extends UseCase<List<DatumEntity>, NoParams> {
+class FetchPastMatchesUseCase extends UseCase<MatchesEntity, NoParams> {
   final MatchesRepository matchesRepository;
   FetchPastMatchesUseCase(this.matchesRepository);
 
