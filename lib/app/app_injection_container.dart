4659fc173c225274c642ed9eec159c0f0e154d58
Mohammad shoaib <eng.mohammadshoaib98@gmail.com>
2024-11-01 11:08:45 +0300

    Expand dependency injection with data sources, repositories, use cases, and BLoC for Matches feature

--- lib/app/app_injection_container.dart
+++ lib/app/app_injection_container.dart
@@ -2,6 +2,13 @@ import 'package:get_it/get_it.dart';
 
 import '../core/network/network_info.dart';
 import '../core/services/api_service.dart';
+import '../features/matches/data/datasource/remote/matches_remote_datasource.dart';
+import '../features/matches/data/repository_imp/matches_repository_impl.dart';
+import '../features/matches/domain/repository/matches_repository.dart';
+import '../features/matches/domain/usecases/fetch_past_matches_use_case.dart';
+import '../features/matches/domain/usecases/fetch_todays_matches_use_case.dart';
+import '../features/matches/domain/usecases/fetch_upcoming_matches_use_case.dart';
+import '../features/matches/presentation/bloc/matches_bloc.dart';
 
 final sl = GetIt.instance;
 
@@ -19,7 +26,22 @@ void core() {
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 }
 
-void dataSources() {}
-void repositories() {}
-void useCases() {}
-void blocs() {}
+void dataSources() {
+  sl.registerFactory<MatchesRemoteDatasource>(
+      () => MatchesRemoteDatesourceImpl(sl(), sl()));
+}
+
+void repositories() {
+  //MatchesRepository
+  sl.registerFactory<MatchesRepository>(() => MatchesRepositoryImpl(sl()));
+}
+
+void useCases() {
+  sl.registerLazySingleton(() => FetchPastMatchesUseCase(sl()));
+  sl.registerLazySingleton(() => FetchTodaysMatchesUseCase(sl()));
+  sl.registerLazySingleton(() => FetchUpcomingMatchesUseCase(sl()));
+}
+
+void blocs() {
+  sl.registerLazySingleton<MatchesBloc>(() => MatchesBloc(sl(), sl(), sl()));
+}
